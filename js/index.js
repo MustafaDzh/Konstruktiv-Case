c96eecf41b6690444446777260da8a655414b88d
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-09-11 11:58:49 +0300

    video

--- js/index.js
+++ js/index.js
@@ -1,3 +1,49 @@
-$(document).ready(function() {
-    
-})
+$(function () {
+    var player = videojs('my-video');
+    var playBtn = $('.play-btn');
+    var pauseBtn = $('.pause-btn');
+    var isPlaying = false;
+
+    function togglePlayPause() {
+        if (isPlaying) {
+            player.pause();
+            playBtn.removeClass('hidden');
+            pauseBtn.addClass('hidden');
+        } else {
+            player.play();
+            playBtn.addClass('hidden');
+            pauseBtn.removeClass('hidden');
+        }
+        isPlaying = !isPlaying;
+    }
+
+    playBtn.click(togglePlayPause);
+    pauseBtn.click(togglePlayPause);
+
+    $('.section_highlights-wrapper--content_top-video').hover(
+        function () {
+            if (isPlaying) {
+                playBtn.addClass('hidden');
+                pauseBtn.removeClass('hidden');
+            } else {
+                playBtn.removeClass('hidden');
+                pauseBtn.addClass('hidden');
+            }
+        },
+        function () {
+            if (isPlaying) {
+                pauseBtn.addClass('hidden');
+            } else {
+                playBtn.addClass('hidden');
+            }
+        }
+    );
+
+    playBtn.removeClass('hidden');
+
+    player.on('ended', function () {
+        pauseBtn.addClass('hidden');
+        playBtn.removeClass('hidden');
+        isPlaying = false;
+    });
+});
